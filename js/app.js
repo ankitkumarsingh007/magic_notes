c77b93594027b642bbaee5c91879a0274cf99103
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2021-12-23 18:40:28 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,17 +3,23 @@
 
  // If user adds a note, add it to the localStorage
  let addBtn = document.getElementById("addBtn");
- addBtn.addEventListener("click", function(e) {
+ addBtn.addEventListener("click", function() {
      let addTxt = document.getElementById("addTxt");
+     let addTitle = document.getElementById("addTitle");
      let notes = localStorage.getItem("notes");
      if (notes == null) {
          notes = [];
      } else {
          notes = JSON.parse(notes);
      }
-     notes.push(addTxt.value);
+     let obj = {
+         title: addTitle.value,
+         text: addTxt.value
+     }
+     notes.push(obj);
      localStorage.setItem("notes", JSON.stringify(notes));
      addTxt.value = "";
+     addTitle.value = "";
      showNotes();
  });
 
@@ -30,8 +36,8 @@
          html += `
             <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 id="card-title">Note ${index + 1}</h5>
-                        <p class="cardTxt"> ${element}</p>
+                        <h5 id="card-title">${element.title}</h5>
+                        <p class="cardTxt"> ${element.text}</p>
                         <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                     </div>
                 </div>`;
@@ -64,9 +70,10 @@
      let inputVal = search.value.toLowerCase();
      let noteCards = document.getElementsByClassName('noteCard');
      Array.from(noteCards).forEach(function(element) {
+         let cardTitle = element.getElementsByTagName("h5")[0].innerText.toLowerCase();
          let cardTxt = element.getElementsByTagName("p")[0].innerText.toLowerCase();
          //here any other query selector is not working efficiently
-         if (cardTxt.includes(inputVal)) {
+         if (cardTxt.includes(inputVal) || cardTitle.includes(inputVal)) {
              element.style.display = "block";
          } else {
              element.style.display = "none";
